303549799601ba84dac7a35e7fe78375fa04f184
ilysblg <ilys.boualagui@esprit.tn>
2022-10-14 08:36:11 +0100

    commit finale

--- ecert/__init__.py
+++ ecert/__init__.py
@@ -7,8 +7,8 @@ from flask_admin import Admin
 from flask_mail import Mail
 from flask_admin.contrib.sqla import ModelView 
 app = Flask(__name__) 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lysyseasgwadjz:48ed62b33d555c7a848c5bc1eb251e8faea81fbb7a0585e1d0557486c31cb252@ec2-54-208-104-27.compute-1.amazonaws.com:5432/d2tonnr2oba59s'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) 
 bcrypt = Bcrypt(app) 
 login_manager = LoginManager(app)
